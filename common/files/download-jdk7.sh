11d10efbd98ce86842a6c57b7c79a9cfb1ee9952
Michael Pope <michael@dtcorp.com.au>
2013-05-23 09:23:55 +1000

    Add security download to java 7

--- common/files/download-jdk7.sh
+++ common/files/download-jdk7.sh
@@ -2,6 +2,8 @@
 #
 # Ref: http://blog.trifork.com/2013/04/02/ansible-example-playbook-to-setup-jenkins-slave/
 #
-
+# By running this script you agree to the terms and conditions set by Oracle
+#
 # Download JDK7
 wget --no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com" http://download.oracle.com/otn-pub/java/jdk/7/$1 -O $1
+wget --no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com" http://download.oracle.com/otn-pub/java/jce/7/UnlimitedJCEPolicyJDK7.zip -O /tmp/UnlimitedJCEPolicyJDK7.zip
